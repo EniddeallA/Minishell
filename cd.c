31e9eb754150acb31039da7f626da031d1fe4fcd
aelkhalo <ekhaluffy@gmail.com>
2022-03-05 09:38:16 +0100

    cd corrected

--- cd.c
+++ cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akhalid <akhalid@student.42.fr>            +#+  +:+       +#+        */
+/*   By: aelkhalo <aelkhalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 05:28:26 by aelkhalo          #+#    #+#             */
-/*   Updated: 2022/03/03 01:35:40 by akhalid          ###   ########.fr       */
+/*   Updated: 2022/03/05 09:37:22 by aelkhalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,8 @@ void	update_pwds(void)
 
 	if (!key_exist("OLDPWD"))
 		add_envv("OLDPWD", "");
+	if (!key_exist("PWD"))
+		add_envv("PWD", "");
 	oldnode = get_node("OLDPWD");
 	if (oldnode && oldnode->value)
 		free(oldnode->value);
